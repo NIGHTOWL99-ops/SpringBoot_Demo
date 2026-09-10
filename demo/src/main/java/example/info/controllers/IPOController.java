8add32f0affea03528a7e47ddb64d603d3c89ca1
NIGHTOWL99-ops <C9893616@GMAIL.COM>
2026-09-10 22:37:56 +0530

    Added an API for returning a success message

--- demo/src/main/java/example/info/controllers/IPOController.java
+++ demo/src/main/java/example/info/controllers/IPOController.java
@@ -1,22 +1,32 @@
 package example.info.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import example.info.pojo.IPO;
 import example.info.service.IPOService;
 
 @RestController
+@RequestMapping("/ipos") 
 public class IPOController {
 	
 	@Autowired
 	IPOService service;
 	
-	@GetMapping("/{id}")
-    public IPO getProductById(@PathVariable Long id) {
-        return service.getIPObyID("xyz");
-    }	
+	/*
+	 * @GetMapping("/{name}") public IPO getProductById(@PathVariable String name) {
+	 * return service.getIPObyID(name); }
+	 */
+	
+	
+	@GetMapping 
+	// Or @PostMapping, depending on your request
+    public ResponseEntity<?> getIpos() {
+        return ResponseEntity.ok("Success");
+    }
+
 
 }
