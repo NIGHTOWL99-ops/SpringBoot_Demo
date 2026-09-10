b68ab9adaa45affd5a1b275b56b23bcdbb44ef06
NIGHTOWL99-ops <C9893616@GMAIL.COM>
2026-09-10 21:39:03 +0530

    Bean Registration Config Class Added

---
+++ demo/src/main/java/com/example/info/BeanRegistrationsConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.info;
+
+import org.springframework.beans.factory.BeanRegistrar;
+import org.springframework.beans.factory.BeanRegistry;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
+
+import example.info.service.IPOService;
+@Configuration
+@Import(BeanRegistrationsConfiguration.MyBeanRegistrar.class)
+public class BeanRegistrationsConfiguration {
+	
+	
+	
+	    static class MyBeanRegistrar implements BeanRegistrar {
+	        @Override
+	        public void register(BeanRegistry registry, Environment env) {
+	            registry.registerBean(IPOService.class);
+	            registry.registerBean("myService2", IPOService.class, spec -> spec
+	                .prototype()
+	                .lazyInit()
+	                .primary()
+	            );
+	        }
+	    }
+	}
+
+
