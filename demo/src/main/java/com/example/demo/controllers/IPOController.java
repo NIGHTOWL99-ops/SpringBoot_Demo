613ed43e54064bdbae9252ba1ba3216050f53100
NIGHTOWL99-ops <C9893616@GMAIL.COM>
2026-09-14 19:32:32 +0530

    Changes related to persistence api - save

--- demo/src/main/java/com/example/demo/controllers/IPOController.java
+++ demo/src/main/java/com/example/demo/controllers/IPOController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,14 +53,19 @@ public class IPOController {
 	  }
 	  
 		
-		  @GetMapping("/uname") //
-		   public List<IPOUser> fetchIPOUser(@RequestParam String uname) 
-		  {
+	@GetMapping("/uname") //
+    public List<IPOUser> fetchIPOUser(@RequestParam String uname) {
 		  
 		 return userrepo.findByName( uname);
 		  
 		  }
-		 
+	
+	@PostMapping
+	public IPOUser addIPOUser(@RequestBody IPOUser ipoUser) {
+			  
+			  
+		 return userrepo.save(ipoUser);
+     }
 	  
 	
 	@GetMapping 
--- demo/src/main/java/com/example/demo/info/InfoOnSpringTransaction.java
+++ demo/src/main/java/com/example/demo/info/InfoOnSpringTransaction.java
@@ -1,17 +1,28 @@
 package com.example.demo.info;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionException;
 import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.SimpleTransactionStatus;
 
 
 @Component
 
 public class InfoOnSpringTransaction implements  PlatformTransactionManager {
+	
+	
+	
+	
+	  @Bean(name = "transactionManager") public PlatformTransactionManager
+	  transactionManager(DataSource dataSource) { return new
+	  DataSourceTransactionManager(dataSource); }
+	 
 	
 	
 	@Override
