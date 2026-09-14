b7fa4f0b22b13479b02aa61c31ee01755112c87c
NIGHTOWL99-ops <C9893616@GMAIL.COM>
2026-09-14 13:44:21 +0530

    Fix for H2 Select Query

--- demo/src/main/java/com/example/demo/controllers/IPOController.java
+++ demo/src/main/java/com/example/demo/controllers/IPOController.java
@@ -1,15 +1,19 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.info.IPOAcceptanceService;
 import com.example.demo.pojo.IPO;
 import com.example.demo.services.IPOService;
+import com.example.demo.stress.IPOUser;
 import com.example.demo.stress.UserRepository;
 
 @RestController
@@ -46,13 +50,15 @@ public class IPOController {
 	  
 	  }
 	  
-		/*
-		 * @GetMapping("/save") //@Transactional public User saveUser(User user) {
-		 * 
-		 * userrepo.save();
-		 * 
-		 * }
-		 */
+		
+		  @GetMapping("/uname") //
+		   public List<IPOUser> fetchIPOUser(@RequestParam String uname) 
+		  {
+		  
+		 return userrepo.findByName( uname);
+		  
+		  }
+		 
 	  
 	
 	@GetMapping 
--- demo/src/main/java/com/example/demo/stress/UserRepository.java
+++ demo/src/main/java/com/example/demo/stress/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.stress;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<IPOUser, Long>{
 
 	
-	
+	List<IPOUser> findByName(String name);
 }
